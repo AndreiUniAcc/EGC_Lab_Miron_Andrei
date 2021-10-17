33903aa87c2e40e603fd43e5425dfc61d8f787e1
Andrei Miron <andrei.miron2@student.usv.ro>
2021-10-17 13:22:36 +0300

    Nu merge momentan, ecran doar alb.

--- First_OpenTK/Program.cs
+++ First_OpenTK/Program.cs
@@ -1,15 +1,54 @@
-﻿using System;
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System;
+
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace First_OpenTK
 {
+    class FirstWindow: GameWindow
+    {
+        public FirstWindow() : base(800, 600)
+        {
+            KeyDown += Keyboard_KeyDown;
+        }
+
+        void Keyboard_KeyDown ( object sender, KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                this.Exit();
+
+            if (e.Key == Key.F1)
+                if (this.WindowState == WindowState.Fullscreen)
+                    this.WindowState = WindowState.Normal;
+                else
+                    this.WindowState = WindowState.Fullscreen;
+
+            if (e.Key == Key.G)
+                GL.ClearColor(Color.DarkSeaGreen);  
+        }
+
+        protected override void OnLoad(EventArgs e)
+        { 
+            GL.ClearColor(Color.MidnightBlue);
+        }
+
+
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            using (FirstWindow EGC = new FirstWindow())
+            {
+                EGC.Run(30.0, 0.0);
+            }
         }
     }
 }
