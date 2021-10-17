3532b50b265573f91bb1a079955a5cc7ca1a0830
Andrei Miron <andrei.miron2@student.usv.ro>
2021-10-17 14:04:33 +0300

    Merge culoarea triunghiului principal. De rezolvat triunghiuri negre lateral.

--- First_OpenTK/Program.cs
+++ First_OpenTK/Program.cs
@@ -2,50 +2,64 @@
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
 using System;
-
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace First_OpenTK
 {
-    class FirstWindow: GameWindow
+    class SimpleWindow : GameWindow
     {
-        public FirstWindow() : base(800, 600)
+        // Constructor.
+        public SimpleWindow() : base(800, 600)
         {
-            KeyDown += Keyboard_KeyDown;
+            KeyDown += KeyBoard_KeyDown;
         }
 
-        void Keyboard_KeyDown ( object sender, KeyboardKeyEventArgs e)
+        void KeyBoard_KeyDown(object sender, KeyboardKeyEventArgs e)
         {
             if (e.Key == Key.Escape)
+            {
                 this.Exit();
+            }
 
-            if (e.Key == Key.F1)
+            if (e.Key == Key.F11)
                 if (this.WindowState == WindowState.Fullscreen)
                     this.WindowState = WindowState.Normal;
                 else
                     this.WindowState = WindowState.Fullscreen;
-
-            if (e.Key == Key.G)
-                GL.ClearColor(Color.DarkSeaGreen);  
         }
 
-        protected override void OnLoad(EventArgs e)
-        { 
-            GL.ClearColor(Color.MidnightBlue);
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
         }
 
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
 
+            GL.Begin(PrimitiveType.Triangles);
 
-    }
-    class Program
-    {
+            GL.Color3(Color.MidnightBlue);
+            GL.Vertex2(-1.0f, 1.0f);
+
+            GL.Color3(Color.SpringGreen);
+            GL.Vertex2(0.0f, -1.0f);
+
+            GL.Color3(Color.Ivory);
+            GL.Vertex2(1.0f, 1.0f);
+
+            GL.End();
+
+            this.SwapBuffers();
+        }
+        [STAThread]
         static void Main(string[] args)
         {
-            using (FirstWindow EGC = new FirstWindow())
+            using (SimpleWindow EGC = new SimpleWindow())
             {
                 EGC.Run(30.0, 0.0);
             }
